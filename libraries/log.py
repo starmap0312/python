f7d43f9092b1dcf2a8aed70360044f57d2fbad99
kuanyu <starmap0312@gmail.com>
2017-03-30 09:20:03 +0800

    add more

--- libraries/log.py
+++ libraries/log.py
@@ -1,22 +1,29 @@
 # Level
 #   DEBUG < INFO < WARNING < ERROR < CRITICAL
-
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# create a console handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+# create a logging format for the console handler
+formatter = logging.Formatter('%(message)s')
+console_handler.setFormatter(formatter)
+
 # create a file handler
 handler = logging.FileHandler('example.log')
 handler.setLevel(logging.INFO)
-
-# create a logging format
+# create a logging format for the file handler
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 
 # add the handlers to the logger
 logger.addHandler(handler)
+logger.addHandler(console_handler)
 
-logger.debug('debug message')
-logger.info('info message')
-logger.error('error message')
+logger.debug('this is a debug message')
+logger.info('this is a info message')
+logger.error('this is a error message')
