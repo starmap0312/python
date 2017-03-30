a60a73eefeb8558f862194f447fa3475de1072d7
kuanyu <starmap0312@gmail.com>
2017-03-30 09:35:12 +0800

    add more

--- libraries/config_parser.py
+++ libraries/config_parser.py
@@ -4,6 +4,9 @@ parser = configparser.ConfigParser()
 parser._interpolation = configparser.ExtendedInterpolation()
 parser.read("example.ini")
 
-print(parser.sections())
-print(parser.get('SectionTwo', 'Param1'))
-print(parser.get('SectionThree', 'Charlie'))
+print(parser.sections())                     # ['SectionOne', 'SectionTwo', 'SectionThree'] 
+print(parser.get('SectionOne', 'Param1'))    # Hello
+print(parser.get('SectionOne', 'Param2'))    # World
+#print(parser.get('SectionOne', 'Param3'))   # NoOptionError: No option 'param3' in section: 'SectionOne'
+print(parser.get('SectionTwo', 'Param1'))    # Hello World
+print(parser.get('SectionThree', 'Charlie')) # One Mississippi
