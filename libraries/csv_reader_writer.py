3705d2e58289ce5256891e227e875329fd7a776f
kuanyu <starmap0312@gmail.com>
2017-08-17 14:56:49 +0800

    add more

--- libraries/csv_reader_writer.py
+++ libraries/csv_reader_writer.py
@@ -3,10 +3,29 @@ import csv
 if __name__ == '__main__':
   # read in a *.csv file
   with open("foo.csv", 'r') as fin:
-      for row in csv.reader(fin): # ['',           'A',              'B',              'C',              'D'             ]
-          print(row)              # ['2016-07-01', '0.858490705656', '-1.13613367816', '-1.35393014638', '-0.46613600423'] 
+      reader = csv.reader(fin)
+      headers = reader.next()
+      print(headers)
+      for row in reader: # ['',           'A',              'B',              'C',              'D'             ]
+          print(row)     # ['2016-07-01', '0.858490705656', '-1.13613367816', '-1.35393014638', '-0.46613600423'] 
 
-  with open('foo2.csv', 'wb') as fout:
+  # read in a *.csv file as DictReader
+  with open("foo.csv", 'r') as fin:
+      reader = csv.DictReader(fin)
+      print(reader.fieldnames) # ['',           'A',              'B',              'C',              'D'             ]
+      for row in reader:
+          print(row['D'])      # -0.46613600423
+
+  # write to a *.tsv file
+  with open('foo.tsv', 'wb') as fout:
+    headers = ['Date', 'A', 'B', 'C', 'D']
     writer = csv.writer(fout, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['', 'A', 'B', 'C', 'D'])
+    writer.writerow(headers)
     writer.writerow(['2016-07-01', '0.858490705656', '-1.13613367816', '-1.35393014638', '-0.46613600423'])
+
+  # write to a *.tsv file via DictWriter
+  with open('foo_dict.csv', 'wb') as fout:
+      headers = ['Date', 'A', 'B', 'C', 'D']
+      writer = csv.DictWriter(fout, fieldnames=headers)
+      writer.writeheader()
+      writer.writerow({'Date': '2016-07-01', 'A': '0.858490705656', 'B': '-1.13613367816', 'C': '-1.35393014638', 'D': '-0.46613600423'})
